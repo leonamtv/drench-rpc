8eeaad60e579aa2aeb14fa2f5e1ca6cb22df0d15
Leonam <leonan.aiirrc02t@gmail.com>
2021-03-07 17:38:49 -0300

    feat: implementing win/over logic

--- node/index.js
+++ node/index.js
@@ -2,14 +2,14 @@ const xmlrpc = require('xmlrpc')
 const drench = require('./core/drench')
 const prompt = require('prompt-sync')();
 
-const d = drench(6, 20)
+// const d = drench(6, 20)
+const d = drench(4, 20)
 d.init()
 
 const LOCAL_SERVER_PORT = 3001
 const REMOTE_SERVER_PORT = 3003
 
 let client = xmlrpc.createClient ({ 
-
     host : '0.0.0.0',
     port : REMOTE_SERVER_PORT,
     path : '/'
@@ -24,17 +24,45 @@ const cliente = ( entry  ) => {
     })
 }
 
+const cliente_end = ( entry  ) => {
+    return new Promise (( resolve, reject ) => {
+        return client.methodCall('end', [ ], (error, value) => {
+            if (error) { return reject(error); }
+            return resolve ( value );
+        })
+    })
+}
+
 const start = () => {
-    setInterval(async () => {
+    let refreshId = setInterval(async () => {
         if ( d.remainingPaints() % 2 == 1 ) {
+            if ( d.isOver () ) {
+                console.log('GAME OVER!!!')
+                process.exit()
+            }
             console.log ('Sua vez!!!')
             console.log ( d.getOptions()) 
-            const entry = prompt('Próximo valor:')
+            const entry = prompt('Próximo valor: ')
             d.updateMatrix ( parseInt(entry) )
-            await cliente( entry )
+            d.print()
+            await cliente( entry ) 
+            if ( d.isOver () ) {
+                console.log('GAME OVER!!!')
+                if (  d.isFull() ) {
+                    console.log('Você ganhou!')
+                }
+                process.exit()
+            }
+        } else {
+            if ( d.isOver () ) {
+                console.log('GAME OVER!!!')
+                if (  d.isFull() ) {
+                    console.log('Você ganhou!')
+                }
+                process.exit()
+            }
         }
-
-    }, 300)
+    }, 500)
 }
 
 start()
@@ -46,7 +74,6 @@ const server = xmlrpc.createServer ({
 })
 
 server.on ( 'update', ( err, params, callback ) => {
-    console.log('chamado')
     d.updateMatrix ( parseInt(params[0])) 
     d.print()
     callback ( null, 'updated' )
@@ -58,5 +85,7 @@ server.on ( 'sync', ( err, params, callback ) => {
     callback ( null, 'synced' )
 })
 
-
-
+process.on('uncaughtException', function (err) {
+    console.error(err.stack);
+    console.log("Node NOT Exiting...");
+  });
--- python/main.py
+++ python/main.py
@@ -1,4 +1,5 @@
-from sys import argv
+from sys import argv 
+from os import _exit
 
 from threading import Thread
 from core.drench import Drench
@@ -11,7 +12,8 @@ import xmlrpc.client
 LOCAL_SERVER_PORT = 3003
 REMOTE_SERVER_PORT = 3001
 
-d = Drench (6, 30)
+# d = Drench (6, 20)
+d = Drench (4, 20)
 
 def server ( functions ) :
     with SimpleXMLRPCServer(('0.0.0.0', LOCAL_SERVER_PORT),requestHandler=RequestHandler, logRequests=False) as server :
@@ -44,11 +46,26 @@ try :
     print ( d )
     while True: 
         if d.remaining_paints () % 2 == 0 :
+            if d.is_over () :
+                print('GAME OVER!!!')
+                _exit(1)
             print('Sua vez!!!')
             print ( d.get_options ()) 
             entry = int ( input ( 'Próximo valor: ' ))
             d.update_matrix ( entry )
+            print ( d )
             client_update ( entry )
+            if d.is_over () :
+                print('GAME OVER!!!')
+                if d.is_full () :
+                    print('Você ganhou!')
+                _exit(1)
+        else :
+            if d.is_over () :
+                print('GAME OVER!!!')
+                if d.is_full () :
+                    print('Você ganhou!')
+                _exit(1)
     
     thread.join()
         
